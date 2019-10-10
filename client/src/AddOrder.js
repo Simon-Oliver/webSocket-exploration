b5db3c87f02250bd5cbf36ab52423069e473a4cf
Simon-Oliver <stauffer.simon@outlook.com>
2019-10-10 21:00:02 +1100

    form input

--- client/src/AddOrder.js
+++ client/src/AddOrder.js
@@ -4,34 +4,24 @@ const client = new W3CWebSocket('ws://192.168.1.11:8000');
 
 class AddOrder extends React.Component {
   state = {
-    orders: []
+    newOrder: {}
+  };
+
+  handleOnChange = e => {
+    const key = e.target.id;
+    const data = e.target.value;
+    this.setState({ newOrder: { [key]: data } });
   };
 
   render() {
     return (
       <div>
-        <div class="row">
-          <form class="col s12">
-            <div class="row">
-              <div class="input-field col s6">
-                <input placeholder="Placeholder" id="first_name" type="text" class="validate" />
-                <label for="first_name">First Name</label>
-              </div>
-              <div class="input-field col s6">
-                <input id="last_name" type="text" class="validate" />
-                <label for="last_name">Last Name</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12">
-                <input id="password" type="password" class="validate" />
-                <label for="password">Password</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12">
-                <input id="email" type="email" class="validate" />
-                <label for="email">Email</label>
+        <div className="row">
+          <form className="col s12" onChange={e => this.handleOnChange(e)}>
+            <div className="row">
+              <div className="input-field col s12">
+                <input id="orderTitle" type="text" className="validate" />
+                <label htmlFor="orderTitle">Order Title</label>
               </div>
             </div>
           </form>
