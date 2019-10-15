11d590781f9f8a641a1910d38675b097819aacd2
Simon-Oliver <stauffer.simon@outlook.com>
2019-10-15 22:38:06 +1100

    added onClick for table row

--- client/src/Components/Order/Item.js
+++ client/src/Components/Order/Item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Item(props) {
   return (
-    <tr className="item">
+    <tr id={props.item.id} className="item" onClick={e => console.log(e.currentTarget.id)}>
       <td className="number">{props.item.qnt}</td>
       <td>{props.item.item}</td>
       <td className="number">{props.item.price}</td>
