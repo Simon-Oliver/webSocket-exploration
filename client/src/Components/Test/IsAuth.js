e62e28c9d2986937a24f96c254f587ae13274800
Simon-Oliver <stauffer.simon@outlook.com>
2019-11-06 09:20:45 +1100

    thinkering with cookies

--- client/src/Components/Test/IsAuth.js
+++ client/src/Components/Test/IsAuth.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
-const client = new W3CWebSocket('ws://192.168.1.11:8080');
 import jwt from 'jsonwebtoken';
+const client = new W3CWebSocket('ws://192.168.1.11:8080');
 
 class IsAuth extends React.Component {
   state = {
