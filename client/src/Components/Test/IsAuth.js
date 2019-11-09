49555bf66f3b518555f979b3f88f64aa6b893d8f
Simon-Oliver <stauffer.simon@outlook.com>
2019-11-09 19:33:31 +0100

    minor changes to auth component

--- client/src/Components/Test/IsAuth.js
+++ client/src/Components/Test/IsAuth.js
@@ -32,7 +32,15 @@ class IsAuth extends React.Component {
     if (this.state.redirect) {
       return <Redirect to={this.state.redirect}></Redirect>;
     }
-    return <div> {this.state.isAuth ? <p>Is Authenticated</p> : <p>Not logged in</p>}</div>;
+    return (
+      <div>
+        {this.state.isAuth ? (
+          <div className="container">
+            <h3>Is auth</h3>
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
 
